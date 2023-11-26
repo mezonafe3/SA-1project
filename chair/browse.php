173ed552273c9c9ee5e0f66d6491b7ff9077e3b1
mezonafe3 <105220171+mezonafe3@users.noreply.github.com>
2023-11-26 17:50:16 +0200

    chair

---
+++ chair/browse.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>Cyborg - Awesome HTML5 Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-cyborg-gaming.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+    <link rel="stylesheet" type="text/css" href="assets/css/div.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
+<!--
+
+TemplateMo 579 Cyborg Gaming
+
+https://templatemo.com/tm-579-cyborg-gaming
+
+-->
+  </head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.php" class="logo">
+                        <img src="assets/images/logo.png" alt="">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Search End ***** -->
+                    <div class="search-input">
+                      <form id="search" action="#">
+                        <input type="text" placeholder="Type Something" id='searchText' name="searchKeyword" onkeypress="handle" />
+                        <i class="fa fa-search"></i>
+                      </form>
+                    </div>
+                    <!-- ***** Search End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="browse.php" class="active">Browse</a></li>
+                        <li><a href="details.php">Details</a></li>
+                        <li><a href="streams.php">Streams</a></li>
+                        <li><a href="profile.php">Profile <img src="assets/images/profile-header.jpg" alt=""></a></li>
+                    </ul>   
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="page-content">
+
+        <div class="container-0">
+      <div class="form-edit">
+        <form id="booking-seat" action="chairs.php" method="POST" >  
+                 <div class="container">
+                    <div id="table-container">
+                     <table class="choose-seat">
+                        <tr ><label for="">Select seat</label></tr>  
+                          <tr></tr>
+                           <tr >  
+                             <td class="row1"  >R1-S1<input type="checkbox" name="arrayOFcheck[]" value="R1-S1" class="s-1" ></td>  
+                             <td class="row1">R1-S2<input type="checkbox" name="arrayOFcheck[]" value="R1-S2"></td> 
+                             <td class="row1">R1-S3<input type="checkbox" name="arrayOFcheck[]" value="R1-S3"></td>
+                             <td class="empty"></td>
+                             <td class="row1">R1-S4<input type="checkbox" name="arrayOFcheck[]" value="R1-S4"></td>
+                             <td class="row1">R1-S5<input type="checkbox" name="arrayOFcheck[]" value="R1-S5"></td>
+                             <td class="row1">R1-S6<input type="checkbox" name="arrayOFcheck[]" value="R1-S6"></td>
+                             <td class="row1">R1-S7<input type="checkbox" name="arrayOFcheck[]" value="R1-S7"></td> 
+                             <td class="empty"></td>
+                             <td class="row1">R1-S8<input type="checkbox" name="arrayOFcheck[]" value="R1-S8"></td> 
+                             <td class="row1">R1-S9<input type="checkbox" name="arrayOFcheck[]" value="R1-S9"></td> 
+                             <td class="row1">R1-S10<input type="checkbox" name="arrayOFcheck[]" value="R1-S10"></td>  
+                            </tr>  
+                            <!-- firstrow -->
+
+    <tr >  
+      <td class="row2">R2-S1<input type="checkbox" name="arrayOFcheck[]" value="R2-S1"></td>  z
+      <td class="row2">R2-S2<input type="checkbox" name="arrayOFcheck[]" value="R2-S2"></td> 
+      <td class="row2">R2-S3<input type="checkbox" name="arrayOFcheck[]" value="R2-S3"></td>
+      <td class="empty"></td>
+      <td class="row2">R2-S4<input type="checkbox" name="arrayOFcheck[]" value="R2-S4"></td>
+      <td class="row2">R2-S5<input type="checkbox" name="arrayOFcheck[]" value="R2-S5"></td>
+      <td class="row2">R2-S6<input type="checkbox" name="arrayOFcheck[]" value="R2-S6"></td>
+      <td class="row2">R2-S7<input type="checkbox" name="arrayOFcheck[]" value="R2-S7"></td>  
+      <td class="empty"></td>
+      <td class="row2">R2-S8<input type="checkbox" name="arrayOFcheck[]" value="R2-S8"></td>
+      <td class="row2">R2-S9<input type="checkbox" name="arrayOFcheck[]" value="R2-S9"></td>
+      <td class="row2">R2-S10<input type="checkbox" name="arrayOFcheck[]" value="R2-S10"></td>  
+   </tr>  
+   <tr>  
+      <td class="row3">R3-S1<input type="checkbox" name="arrayOFcheck[]" value="R3-S1"></td>  
+      <td class="row3">R3-S2<input type="checkbox" name="arrayOFcheck[]" value="R3-S2"></td> 
+      <td class="row3">R3-S3<input type="checkbox" name="arrayOFcheck[]" value="R3-S3"></td>
+      <td class="empty"></td>
+      <td class="row3">R3-S4<input type="checkbox" name="arrayOFcheck[]" value="R3-S4"></td>
+      <td class="row3">R3-S5<input type="checkbox" name="arrayOFcheck[]" value="R3-S5"></td>
+      <td class="row3">R3-S6<input type="checkbox" name="arrayOFcheck[]" value="R3-S6"></td>
+      <td class="row3">R3-S7<input type="checkbox" name="arrayOFcheck[]" value="R3-S7"></td>  
+      <td class="empty"></td>
+      <td class="row3">R3-S8<input type="checkbox" name="arrayOFcheck[]" value="R3-S8"></td>
+      <td class="row3">R3-S9<input type="checkbox" name="arrayOFcheck[]" value="R3-S9"></td>
+      <td class="row3">R3-S10<input type="checkbox" name="arrayOFcheck[]" value="R3-S10"></td> 
+   </tr>  
+   <tr >  
+   <td class="row4">R3-S1<input type="checkbox" name="arrayOFcheck[]" value="R4-S1"></td>  
+      <td class="row4">R4-S2<input type="checkbox" name="arrayOFcheck[]" value="R4-S2"></td> 
+      <td class="row4">R4-S3<input type="checkbox" name="arrayOFcheck[]" value="R4-S3"></td>
+      <td class="empty"></td>
+      <td class="row4">R4-S4<input type="checkbox" name="arrayOFcheck[]" value="R4-S4"></td>
+      <td class="row4">R4-S5<input type="checkbox" name="arrayOFcheck[]" value="R4-S5"></td>
+      <td class="row4">R4-S6<input type="checkbox" name="arrayOFcheck[]" value="R4-S6"></td>
+      <td class="row4">R4-S7<input type="checkbox" name="arrayOFcheck[]" value="R4-S7"></td>  
+      <td class="empty"></td>
+      <td class="row4">R4-S8<input type="checkbox" name="arrayOFcheck[]" value="R4-S8"></td>
+      <td class="row4">R4-S9<input type="checkbox" name="arrayOFcheck[]" value="R4-S9"></td>
+      <td class="row4">R4-S10<input type="checkbox" name="arrayOFcheck[]" value="R4-S10"></td> 
+   </tr>  
+</table>  
+<div class="sub-butt-d">  
+      <input class="sub-butt" type="submit" value="submit" name="sub">
+</div>  
+</div>
+<!-- selection -->
+<div class="alllist">
+<div class="custom-select" style="width:200px;">
+									
+     <select name="movie-select">
+     <option>Select Movie</option>
+			 <?php
+			foreach($mov as $m){
+			  ?>
+        <option value="<?php echo $m["Movie_Title"] ?>"><?php echo $m["Movie_Title"] ?></option>
+				<?php
+				}?>                                      
+     </select>
+      </div>
+     <div class="custom-select" style="width:200px;">
+     <select name="day-select">
+    <option>Select Day</option>
+    <option value="13-To Day">13-To Day</option>
+    <option value="14-Tomorrow">14-Tomorrow</option>
+    <option value="15-sunday">15-sunday</option>
+    <option value="6-monday">16-monday</option>
+    <option value="17-tuesday">17-tuesday</option>
+    <option value="8-wednesday">18-wednesday</option>
+    <option value="19-thurusda">19-thurusday</option>
+     </select>
+     </div>
+     <div class="custom-select" style="width:200px;">
+    <select name="time-select">
+    <option >Select Time </option>
+    <option value="H-5/9:am">H-5/9:am</option>
+    <option value="H-3/12:am">H-3/12:am</option>
+    <option value="H-2/3:am">H-2/3:am</option>
+    </select>
+    </div>
+    <div class="custom-select" style="width:200px;">
+    <select name="food-select">
+    <option >Select food </option>
+    <option value="Popcorn&drink">Popcorn&drink</option>
+    <option value="Popcorn&chips">Popcorn&chips</option>
+    <option value="Chips&drink">Chips&drink</option>
+    <option value="drink">drink</option>
+    </select>
+    </div>
+</div>
+</div>
+</form> 
+</div>
+</div>
+       
+<div class="distance">
+
+</div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  
+ <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p>Copyright © 2036 <a href="#">Cyborg Gaming</a> Company. All rights reserved. 
+          
+          <br>Design: <a href="https://templatemo.com" target="_blank" title="free CSS templates">TemplateMo</a></p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  <script src="assets/js/isotope.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/tabs.js"></script>
+  <script src="assets/js/popup.js"></script>
+  <script src="assets/js/custom.js"></script>
+
+
+  </body>
+
+</html>
